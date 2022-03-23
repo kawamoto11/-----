a0bf815093016e80f4ac6151b1a12d144d640fbd
hspoyo <po1poke@yahoo.co.jp>
2022-03-23 20:40:42 +0900

    a

--- lib/result.dart
+++ lib/result.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_33/beginner.dart';
 
+import 'FrontButton.dart';
 import 'background.dart';
 
 String exptext1 = '初代大統領のジョージワシントンにちなんでるよ';
@@ -14,10 +15,35 @@ class Resultz extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('${resulttext}問正解'),
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.4,
+          ),
+          Container(
+            child: Text(
+              '3問中${resulttext}問正解！',
+              style: TextStyle(fontSize: 50),
+            ),
+          ),
+          Container(
+              height: height,
+              width: width,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey, //色
+                  spreadRadius: 3,
+                  blurRadius: 3,
+                  offset: Offset(1, 1),
+                ),
+              ], color: Colors.white, borderRadius: BorderRadius.circular(5)),
+              child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    '最初の画面に戻る',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ))),
+        ],
       ),
     );
   }
